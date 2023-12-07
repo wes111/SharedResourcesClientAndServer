9af349a7435090efa305f1480b1abec1f447ed4d
Wesley Luntsford <wesluntsford11@gmail.com>
2023-12-07 07:59:16 -0600

    Add Appwrite to Package.swift. (Might be failing?)

--- Package.swift
+++ Package.swift
@@ -10,12 +10,18 @@ let package = Package(
         .library(
             name: "SharedResourcesClientAndServer",
             targets: ["SharedResourcesClientAndServer"]),
+    ], 
+    dependencies: [
+        .package(url: "git@github.com:appwrite/sdk-for-apple.git", from: "4.0.2"),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "SharedResourcesClientAndServer"),
+            name: "SharedResourcesClientAndServer",
+            dependencies: [.product(name: "", package: "sdk-for-apple")]
+        ),
+        
         .testTarget(
             name: "SharedResourcesClientAndServerTests",
             dependencies: ["SharedResourcesClientAndServer"]),
