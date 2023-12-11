0abce5d7ebc9dd1c8139d142bc1ed547c522312e
Wesley Luntsford <wesluntsford11@gmail.com>
2023-12-10 18:19:52 -0600

    Add queryName

--- Sources/SharedResourcesClientAndServer/Models/UniqueAccountField.swift
+++ Sources/SharedResourcesClientAndServer/Models/UniqueAccountField.swift
@@ -10,4 +10,15 @@ import Foundation
 // Required by Appwrite to be unique for each account.
 public enum UniqueAccountField {
     case username, email, phone
+    
+    public var queryName: String {
+        switch self {
+        case .username:
+            "username"
+        case .email:
+            "email"
+        case .phone:
+            "phone"
+        }
+    }
 }
