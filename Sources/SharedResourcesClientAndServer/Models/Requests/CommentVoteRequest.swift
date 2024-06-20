307aa89c5e4d2a8950946dadd87562fbbacc8374
Wesley Luntsford <wesluntsford11@gmail.com>
2024-06-20 08:32:12 -0500

    Add CommentVoteRequest

---
+++ Sources/SharedResourcesClientAndServer/Models/Requests/CommentVoteRequest.swift
@@ -0,0 +1,20 @@
+//
+//  File.swift
+//  
+//
+//  Created by Wesley Luntsford on 6/20/24.
+//
+
+import Foundation
+
+public struct CommentVoteRequest: Codable {
+    public let commentId: String
+    public let userId: String
+    public let vote: VoteType
+    
+    public init(commentId: String, userId: String, vote: VoteType) {
+        self.commentId = commentId
+        self.userId = userId
+        self.vote = vote
+    }
+}
---
+++ Sources/SharedResourcesClientAndServer/Models/VoteType.swift
@@ -0,0 +1,12 @@
+//
+//  File.swift
+//  
+//
+//  Created by Wesley Luntsford on 6/20/24.
+//
+
+import Foundation
+
+public enum VoteType: Codable {
+    case up, down
+}
