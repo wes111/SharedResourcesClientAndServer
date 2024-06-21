1cfa59f3a896f7e313b91b2ed82904deadeb0c99
Wesley Luntsford <wesluntsford11@gmail.com>
2024-06-20 19:22:28 -0500

    Add CommentVoteDTO

---
+++ Sources/SharedResourcesClientAndServer/Models/Responses/CommentVoteDTO.swift
@@ -0,0 +1,23 @@
+//
+//  File.swift
+//  
+//
+//  Created by Wesley Luntsford on 6/20/24.
+//
+
+import Foundation
+
+// The CommentVote object received from the Appwrite database.
+public struct CommentVoteDTO: Decodable {
+    public let id: String
+    public let creationDate: DateWrapper
+    public let userId: String
+    public let commentId: String
+    public let vote: VoteType
+    
+    enum CodingKeys: String, CodingKey {
+        case userId, commentId, vote
+        case id = "$id"
+        case creationDate = "$createdAt"
+    }
+}
